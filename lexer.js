7fc5d3afe95c01892f77a6bb848dc53a6ca95f2c
kchoo <kchoodev@gmail.com>
2018-02-28 21:19:53 -0600

    Update lexer.js

--- lexer.js
+++ lexer.js
@@ -1,4 +1,5 @@
 // decided that the first pass should just be an interpreter in JS, which will make implementing modules, filesystem, etc. a lot easier
+// using this as a reference: http://lisperator.net/pltut/parser/
 
 if (process.argv.length !== 3) {
   console.log('Usage: node lexer.js <file>.kch');
@@ -12,6 +13,3 @@ const fs = require('fs');
 
 const reader = rl.createInterface({ input: fs.createReadStream(filename); });
 
-reader.on('line', function (line) {
-  console.log('Line from file:', line);
-});
